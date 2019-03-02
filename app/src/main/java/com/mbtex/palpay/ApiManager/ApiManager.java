a4a62fd4926fe58649bd8e07f2608453b11b48cf
mohamedb <mmb186@munca>
2019-03-02 11:43:25 -0330

    Added Base ApiManager class, and authorization extension and its methods

---
+++ app/src/main/java/com/mbtex/palpay/ApiManager/ApiManager.java
@@ -0,0 +1,18 @@
+package com.mbtex.palpay.ApiManager;
+import android.os.AsyncTask;
+
+/**
+ *
+ */
+public class ApiManager extends AsyncTask<String, Integer, String> {
+
+    final static String BASEURL = "https://friend-pay.heroku.com/";
+
+
+    public ApiManager() { }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/mbtex/palpay/ApiManager/AuthorizationApiManager.java
@@ -0,0 +1,30 @@
+package com.mbtex.palpay.ApiManager;
+
+public class AuthorizationApiManager extends ApiManager {
+
+    public AuthorizationApiManager() {}
+
+
+    public String loginUser(String username, String password) {
+        return "";
+    }
+
+    public String registerUser(String username, String email, String password,
+                               String firstname, String lastname)
+    {
+        return "";
+    }
+
+    public Boolean logoutUser (String username)
+    {
+        return false;
+    }
+
+    public String refereshToken()
+    {
+        return "";
+    }
+
+
+
+}
