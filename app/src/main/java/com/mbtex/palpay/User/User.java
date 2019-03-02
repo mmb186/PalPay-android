facff2495e8125606441a5bf2bf16a5f8baf53e9
mohamedb <mmb186@munca>
2019-03-02 11:22:31 -0330

    Implemented first iteration of User singleton

--- app/src/main/java/com/mbtex/palpay/User/User.java
+++ app/src/main/java/com/mbtex/palpay/User/User.java
@@ -1,9 +1,52 @@
+package com.mbtex.palpay.User;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * User Singleton
  */
 public class User {
+
+    private String auth_token;
+    private String username;
+    private String email;
+    private String display_picture_url ;
+    private static User active_user;
+
+
+    private User(String username, String token, String display_picture_url, String email)
+    {
+        this.auth_token = token;
+        this.username = username;
+        this.display_picture_url = display_picture_url;
+        this.email = email;
+    }
+
+    static User getUser(String username, String token, String display_picture_url, String email)
+    {
+        try {
+            active_user = new User(username, token, display_picture_url, email);
+        } catch(Exception e) {
+            throw new RuntimeException("Create User Excpetion");
+        }
+
+        return active_user;
+    }
+
+    static User getUser() {
+        return active_user;
+    }
+
+    public String getAuthToken() {
+        return auth_token;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
 
 
 }
