575449c40d7e8af4e48481108d71855d9fa299cf
mohamedb <mmb186@munca>
2019-03-25 20:33:02 -0230

    Updated UI for for recycler View and started TabTransaction class

---
+++ app/src/main/java/com/mbtex/palpay/Tabs/TabTransaction.java
@@ -0,0 +1,13 @@
+package com.mbtex.palpay.Tabs;
+
+class TabTransaction {
+    private String status;
+    private String type;
+    private float amount;
+    private int _id;
+
+    public static String TAB_APPROVED = "APPROVED";
+    public static String TAB_PENDING = "PENDING";
+    public static String TAB_INACTIVE = "INACTIVE";
+
+}
--- app/src/main/java/com/mbtex/palpay/Tabs/TabTransactionRecyclerViewAdapter.java
+++ app/src/main/java/com/mbtex/palpay/Tabs/TabTransactionRecyclerViewAdapter.java
@@ -1,5 +1,22 @@
 package com.mbtex.palpay.Tabs;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class TabTransactionRecyclerViewAdapter {
     private static final String TAG = "TabTransactionRecycler";
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView image;
+        TabTransaction tab_transaction;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
