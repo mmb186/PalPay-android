94c51914eddf60871863e89a1df298cde9ba8f88
aidan <aidan.gillard96@gmail.com>
2019-03-26 19:16:17 -0230

    FFF

--- app/src/main/java/com/mbtex/palpay/DetailTabView.java
+++ app/src/main/java/com/mbtex/palpay/DetailTabView.java
@@ -1,28 +1,42 @@
 package com.mbtex.palpay;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.mbtex.palpay.User.User;
 
+import org.json.JSONObject;
+
 public class DetailTabView extends AppCompatActivity {
+    Dialog myDialog;
     User current_user;
+
+    protected void submit_transaction(double amount, String transaction_type){
+        JSONObject new_transaction_data = new JSONObject();
+        try {
+            new_transaction_data.put("transaction_type", transaction_type);
+            new_transaction_data.put("amount", amount);
+        }
+        catch(Exception e){}
+    }
 
     private void registerClickListeners() {
         final DetailTabView localContext = this;
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_new_transaction);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                show_transaction_popup(view);
             }
         });
     }
@@ -38,18 +52,20 @@ public class DetailTabView extends AppCompatActivity {
         current_user = getIntent().getExtras().getParcelable("current_user");
         int tab_id = getIntent().getExtras().getInt("tab_id");
 
+        User current_user;
+        myDialog = new Dialog(this);
     }
 
-    public void newTransactionPopup(View v) {
+    public void show_transaction_popup(View v) {
         TextView txtClose;
-        ToggleButton btnToggle;
-        EditText editAmount;
+        final ToggleButton btnToggle;
+        final EditText editAmount;
         Button btnConfirm;
-        boolean pay = true;
         myDialog.setContentView(R.layout.create_transaction_popup);
-        txtClose = (TextView) myDialog.findViewByID(R.id.txtClose);
-        btnToggle = (ToggleButton) myDialog.findViewByID(R.id.btnPayLoan);
-        editAmount = (EditText) myDialog.findViewByID(R.id.txtAmount);
+        txtClose = (TextView) myDialog.findViewById(R.id.txtClose);
+        btnToggle = (ToggleButton) myDialog.findViewById(R.id.btnPayLoan);
+        editAmount = (EditText) myDialog.findViewById(R.id.txtAmount);
+        btnConfirm = (Button) myDialog.findViewById(R.id.btnConfirm);
         txtClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,22 +76,37 @@ public class DetailTabView extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
-                    isChecked = false;
-                    pay = false;
+                    isChecked = false; //false means loan or withdrawal
                 }
                 else{
-                    isChecked = true;
-                    pay = true;
+                    isChecked = true;   //if true it means they are making a payment or deposit
                 }
             }
         });
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                if(submit_transaction()) {
-                    myDialog.dismiss();
+                String amount = editAmount.getText().toString();
+                String type;
+                double dubAmount;
+                if(btnToggle.isChecked()){
+                    type = "DEPOSIT";
                 }
+                else{
+                    type = "WITHDRAW";
+                }
+                try{
+                    dubAmount = Double.parseDouble(amount);
+                    if(dubAmount >= 0.01){
+                        submit_transaction(dubAmount, type);
+                        myDialog.dismiss();
+                    }
+                }
+                catch(Exception e){}
+
+
             }
-        }
+        });
+        myDialog.show();
     }
 }
